1a7618514cf87f8b6c237540163053c9c2ec0eff
gustahxn <guenrik2007@gmail.com>
2025-09-16 17:58:01 -0300

    Configs + Files created

---
+++ calcImc/src/App.jsx
@@ -0,0 +1,14 @@
+
+import InputForm from './components/InputForm'; 
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="p-6">
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ calcImc/src/components/InputForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const InputForm = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="space-x-4">
+        <input type="text" className="border border-gray-300 p-2 rounded" placeholder="Digite algo..." />
+        <input type="text" className="border border-gray-300 p-2 rounded" placeholder="Digite algo..." />
+      </div>
+    </div>
+  );
+};
+
+export default InputForm;
