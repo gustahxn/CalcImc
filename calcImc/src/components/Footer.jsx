6d5142ee72ff1b1b7e120ed4b18a7f64c15760d8
gustahxn <guenrik2007@gmail.com>
2025-10-05 11:44:09 -0300

    Refatora componente Footer para melhorar a legibilidade do código

--- calcImc/src/components/Footer.jsx
+++ calcImc/src/components/Footer.jsx
@@ -1,13 +1,13 @@
 const Footer = () => {
-    return (
-         <footer className="bg-gray-900 text-gray-300 py-8 px-6">
-        <div className="max-w-6xl mx-auto text-center">
-          <p className="text-sm">
-            © 2025 Soro Fitness. Desenvolvido em Sorocaba, SP,{" "}
-            <span className="italic font-bold">Brasil</span>
-          </p>
-        </div>
-      </footer>
-    )
-}
+  return (
+    <footer className="bg-gray-900 text-gray-300 py-8 px-6">
+      <div className="max-w-6xl mx-auto text-center">
+        <p className="text-sm">
+          © 2025 Soro Fitness. Desenvolvido em Sorocaba, SP,{" "}
+          <span className="italic font-bold">Brasil</span>
+        </p>
+      </div>
+    </footer>
+  );
+};
 export default Footer;
