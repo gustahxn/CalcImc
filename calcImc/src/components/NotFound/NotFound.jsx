c313d3b0b12e2745666af91515f73d0a737bf3de
gustahxn <guenrik2007@gmail.com>
2025-10-15 17:35:45 -0300

    refatoração: substitui botão por link de navegação na página NotFound

--- calcImc/src/components/NotFound/NotFound.jsx
+++ calcImc/src/components/NotFound/NotFound.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Header";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
@@ -7,18 +8,18 @@ const NotFound = () => {
       <h1 className="text-2xl font-bold mb-4 mt-20">
         Poxa... o conteúdo que você procura não está aqui :(
       </h1>
-      <img  
+      <img
         src="https://www.iconpacks.net/icons/2/free-sad-face-icon-2692-thumb.png"
         alt="404"
         className="w-32 h-32 mb-4"
       />
 
-      <button
-        onClick={() => (window.location.href = "/")}
-        className="px-4 py-2 bg-black text-white rounded hover:bg-blue-600"
+      <Link
+        to={"/calculator"}
+        className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-lg transition transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring-blue-300 shadow-lg"
       >
-        Tela inicial
-      </button>
+        Página Inicial
+      </Link>
     </div>
   );
 };
