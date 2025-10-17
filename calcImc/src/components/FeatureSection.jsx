888a9842b422cf562fb7c1080273bace0c002e8f
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-17 10:06:06 -0300

    Update: Headings minor changes on Feature section

---
+++ calcImc/src/components/FeatureSection.jsx
@@ -0,0 +1,7 @@
+const FeatureSection = () => {
+    return (
+        <h1>Oi</h1>
+    )
+}
+
+export default FeatureSection
--- calcImc/src/routes/Routes.jsx
+++ calcImc/src/routes/Routes.jsx
@@ -56,7 +56,7 @@ const AppRoutes = ({ setImc, imc }) => {
           <div class="py-4">
       <div class="max-w-6xl mx-auto">
         <div class="max-w-3xl mx-auto mb-16 text-center">
-          <h2 class="text-slate-900 text-3xl lg:text-4xl font-bold text-center mb-4 leading-relaxed">Descubra <span className="underline decoration-4 decoration-blue-600 font-bold">mais</span> sobre tudo que temos a oferece!</h2>
+          <h2 class="text-slate-900 text-3xl lg:text-4xl font-bold text-center mb-4 leading-relaxed">Descubra <span className="underline italic decoration-8 decoration-blue-600 font-bold">mais</span> sobre tudo que temos a oferece!</h2>
           <p class="text-slate-600 text-sm leading-relaxed">Mergulhe nesse mundo de vastas oportunidades, dicas de cuidado e planeje seu próximo passo na busca por uma saúde melhor. Seja <span className="text-blue-600 font-bold hover:text-blue-800">SoroFitness</span></p>
         </div>
 
