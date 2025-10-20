bd013b751965fa37fe47f0bad24d33995fcec914
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-20 07:37:13 -0300

    Update: H2 style change

--- calcImc/src/components/FeatureSection.jsx
+++ calcImc/src/components/FeatureSection.jsx
@@ -5,7 +5,7 @@ const FeatureSection = () => {
         <div className="max-w-6xl mx-auto px-6">
           <div className="max-w-3xl mx-auto mb-16 text-center">
             <h2 className="text-slate-900 text-3xl lg:text-4xl font-bold text-center mb-4 leading-relaxed">
-              Descubra <span className="italic font-bold">mais</span> sobre tudo que temos a oferecer!
+              Descubra mais sobre tudo que temos a oferecer!
             </h2>
             <p className="text-slate-600 text-sm leading-relaxed">
               Mergulhe nesse mundo de vastas oportunidades, dicas de cuidado e planeje seu próximo passo na busca por uma saúde melhor. Seja <span className="font-bold hover:text-blue-600">SoroFitness</span>
