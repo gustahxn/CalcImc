9843c6579320a6469be06ae0e880c7c4994b2947
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-21 11:41:11 -0300

    Update: mudanças visuais

--- calcImc/src/components/Home.jsx
+++ calcImc/src/components/Home.jsx
@@ -87,13 +87,13 @@ const Home = () => {
             <div className="flex flex-col sm:flex-row gap-4 justify-center items-center mt-8 pt-4">
               <Link
                 to="/calculator"
-                className="bg-blue-600 hover:bg-blue-700 transition transform hover:scale-105 text-white px-8 py-4 rounded-full font-semibold shadow-lg tracking-wide w-48 text-center"
+                className="bg-blue-400 hover:bg-blue-500 transition transform hover:scale-105 text-white px-8 py-4 rounded-3xl font-semibold shadow-lg tracking-wide w-48 text-center"
               >
                 Calcular Agora
               </Link>
               <button
                 onClick={scrollToInfo}
-                className="border-2 border-white bg-white text-slate-900 hover:bg-blue-50 px-8 py-4 rounded-full font-semibold shadow-lg tracking-wide w-48 text-center"
+                className="bg-blue-400 text-white hover:bg-blue-500 transition transform hover:scale-105 px-8 py-4 rounded-3xl font-semibold shadow-lg tracking-wide w-48 text-center"
               >
                 Saiba Mais
               </button>
