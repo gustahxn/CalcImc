43f33200a86f54e554679959835ee56ae27bf387
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-22 08:39:50 -0300

    Feat: Novo footer

--- calcImc/src/components/Footer.jsx
+++ calcImc/src/components/Footer.jsx
@@ -1,34 +1,11 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { auth } from "../firebaseConfig";
-import { onAuthStateChanged, signOut } from "firebase/auth";
 const Footer = () => {
   return (
-    <footer className="tracking-wide bg-gradient-to-br from-gray-100 via-blue-50 to-white pt-2 pb-2 px-10 border-t border-slate-200">
-      <div className="max-w-screen-xl mx-auto text-center justify-center items-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 pt-12">
-          <div>
-            <Link to="/" className="text-slate-700 font-medium text-base mb-2">Início</Link>
-          </div>
-
-          <div>
-            <Link to="/calculator" className="text-slate-700 font-medium text-base mb-2">Calculadora</Link>
-          </div>
-
-          <div>
-            <Link to="/login" className="text-slate-700 font-medium text-base mb-2">Entrar</Link>
-          </div>
-
-          <div>
-            <Link to="/signup" className="text-slate-700 font-medium text-base mb-2">Registrar</Link>
-          </div>
-        </div>
-
-        <div className="text-left border-gray-700 mb-4 pt-6 mt-8">
-          <p className="text-slate-700 text-sm">
-            © SoroFitness - Brasil. Todos os direitos reservados.
-          </p>
-        </div>
+    <footer className="bg-gray-900 text-gray-300 py-6 px-6">
+      <div className="max-w-6xl mx-auto text-center">
+        <p className="text-sm">
+          © 2025 Soro Fitness. Desenvolvido em Sorocaba, SP,{" "}
+          <span className="italic font-bold">Brasil</span>
+        </p>
       </div>
     </footer>
   );
