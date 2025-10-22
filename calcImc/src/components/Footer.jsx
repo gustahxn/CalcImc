0d642615e70d0d38c8c74bf5cbb357d4ee1dcf54
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-22 08:25:14 -0300

    Update: Footer links alterados

--- calcImc/src/components/Footer.jsx
+++ calcImc/src/components/Footer.jsx
@@ -1,27 +1,31 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { auth } from "../firebaseConfig";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 const Footer = () => {
   return (
-    <footer class="tracking-wide bg-gradient-to-br from-slate-700 via-slate-800 to-slate-900 pt-2 pb-2 px-10">
-      <div class="max-w-screen-xl mx-auto text-center justify-center items-center">
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 pt-12">
+    <footer className="tracking-wide bg-gradient-to-br from-slate-700 via-slate-800 to-slate-900 pt-2 pb-2 px-10">
+      <div className="max-w-screen-xl mx-auto text-center justify-center items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 pt-12">
           <div>
-            <h4 class="text-slate-400 font-medium text-base mb-2">Home</h4>
+            <Link to="/Home" className="text-slate-400 font-medium text-base mb-2">Home</Link>
           </div>
 
           <div>
-            <h4 class="text-slate-400 font-medium text-base mb-2">Calculadora</h4>
+            <h4 className="text-slate-400 font-medium text-base mb-2">Calculadora</h4>
           </div>
 
           <div>
-            <h4 class="text-slate-400 font-medium text-base mb-2">Entrar</h4>
+            <h4 className="text-slate-400 font-medium text-base mb-2">Entrar</h4>
           </div>
 
           <div>
-            <h4 class="text-slate-400 font-medium text-base mb-2">Registro</h4>
+            <h4 className="text-slate-400 font-medium text-base mb-2">Registro</h4>
           </div>
         </div>
 
-        <div class="border-t text-left border-gray-700 pt-6 mt-8">
-          <p class="text-slate-400 text-sm">
+        <div className="border-t text-left border-gray-700 pv-4 pt-6 mt-8">
+          <p className="text-slate-400 text-sm">
             © SoroFitness - Brasil. Todos os direitos reservados.
           </p>
         </div>
