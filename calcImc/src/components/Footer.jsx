a13dc81e9a3760641d5f72b57ddd1c5b75db7385
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-22 08:28:14 -0300

    Update: Links no footer corrigidos

--- calcImc/src/components/Footer.jsx
+++ calcImc/src/components/Footer.jsx
@@ -8,23 +8,23 @@ const Footer = () => {
       <div className="max-w-screen-xl mx-auto text-center justify-center items-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 pt-12">
           <div>
-            <Link to="/Home" className="text-slate-400 font-medium text-base mb-2">Home</Link>
+            <Link to="/" className="text-slate-400 font-medium text-base mb-2">Início</Link>
           </div>
 
           <div>
-            <h4 className="text-slate-400 font-medium text-base mb-2">Calculadora</h4>
+            <Link to="/calculator" className="text-slate-400 font-medium text-base mb-2">Calculadora</Link>
           </div>
 
           <div>
-            <h4 className="text-slate-400 font-medium text-base mb-2">Entrar</h4>
+            <Link to="/login" className="text-slate-400 font-medium text-base mb-2">Entrar</Link>
           </div>
 
           <div>
-            <h4 className="text-slate-400 font-medium text-base mb-2">Registro</h4>
+            <Link to="/signup" className="text-slate-400 font-medium text-base mb-2">Registrar</Link>
           </div>
         </div>
 
-        <div className="border-t text-left border-gray-700 pv-4 pt-6 mt-8">
+        <div className="border-t text-left border-gray-700 mb-4 pt-6 mt-8">
           <p className="text-slate-400 text-sm">
             © SoroFitness - Brasil. Todos os direitos reservados.
           </p>
