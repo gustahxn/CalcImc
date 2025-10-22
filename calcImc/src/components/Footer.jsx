d01d4a55b873be41d7351d7faaa054ccdc933b4a
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-22 09:18:49 -0300

    Update: footer font alterado

--- calcImc/src/components/Footer.jsx
+++ calcImc/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-b from-gray-900 to-blue-900 text-gray-300 py-6 px-6">
+    <footer className="bg-gray-900 text-gray-400 py-6 px-6">
       <div className="max-w-6xl mx-auto text-center">
-        <p className="text-sm font-medium">
+        <p className="text-sm font-medium tracking-wide">
           © 2025 Soro Fitness. Desenvolvido em Sorocaba, SP,{" "}
           <span className="italic font-bold">Brasil</span>
         </p>
