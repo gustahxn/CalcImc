a1f0d15935ecd65129e93fb82f508edd24c4b397
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-22 08:10:01 -0300

    Update: Footer alterado

--- calcImc/src/components/Footer.jsx
+++ calcImc/src/components/Footer.jsx
@@ -1,27 +1,30 @@
 const Footer = () => {
   return (
-    <footer class="tracking-wide bg-gradient-to-br from-black via-black to-blue-700 px-8 sm:px-12 pt-16 pb-8">
-      <div class="grid min-[1200px]:grid-cols-3 gap-12 xl:gap-16">
-
-        <div class="min-[1200px]:col-span-2 grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
+    <footer class="tracking-wide bg-gradient-to-br from-black via-black to-blue-600 pt-8 pb-6 px-10">
+      <div class="max-w-screen-xl mx-auto">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           <div>
-            <h4 class="text-white font-medium text-base">Services</h4>
+            <h4 class="text-white font-medium text-base mb-6">Home</h4>
           </div>
 
           <div>
-            <h4 class="text-white font-medium text-base">Platforms</h4>
+            <h4 class="text-white font-medium text-base mb-6">Calculadora</h4>
           </div>
 
           <div>
-            <h4 class="text-white font-medium text-base">Company</h4>
+            <h4 class="text-white font-medium text-base mb-6">Entrar</h4>
+          </div>
+
+          <div>
+            <h4 class="text-white font-medium text-base mb-6">Registro</h4>
           </div>
         </div>
-      </div>
 
-      <hr class="mt-10 mb-8 border-gray-600" />
-
-      <div class="flex flex-wrap max-md:flex-col gap-4">
-        <p class="text-slate-400 text-sm md:ml-auto">© SoroFitness - Brasil. Todos os direitos reservados.</p>
+        <div class="border-t text-left border-gray-700 pt-6 mt-8">
+          <p class="text-slate-400 text-sm">
+            © SoroFitness - Brasil. Todos os direitos reservados.
+          </p>
+        </div>
       </div>
     </footer>
   );
