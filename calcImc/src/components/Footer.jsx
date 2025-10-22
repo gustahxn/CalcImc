cb9cd8770794f86f325de8a7f53da45ddc6664b6
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-22 08:15:25 -0300

    Update: Footer font e padding alterado

--- calcImc/src/components/Footer.jsx
+++ calcImc/src/components/Footer.jsx
@@ -1,22 +1,22 @@
 const Footer = () => {
   return (
-    <footer class="tracking-wide bg-gradient-to-br from-slate-700 via-slate-800 to-black pt-8 pb-6 px-10">
+    <footer class="tracking-wide bg-gradient-to-br from-slate-700 via-slate-800 to-black pt-6 pb-6 px-10">
       <div class="max-w-screen-xl mx-auto">
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           <div>
-            <h4 class="text-white font-medium text-base mb-6">Home</h4>
+            <h4 class="text-gray-800 font-medium text-base mb-6">Home</h4>
           </div>
 
           <div>
-            <h4 class="text-white font-medium text-base mb-6">Calculadora</h4>
+            <h4 class="text-gray-800 font-medium text-base mb-6">Calculadora</h4>
           </div>
 
           <div>
-            <h4 class="text-white font-medium text-base mb-6">Entrar</h4>
+            <h4 class="text-gray-800 font-medium text-base mb-6">Entrar</h4>
           </div>
 
           <div>
-            <h4 class="text-white font-medium text-base mb-6">Registro</h4>
+            <h4 class="text-gray-800 font-medium text-base mb-6">Registro</h4>
           </div>
         </div>
 
