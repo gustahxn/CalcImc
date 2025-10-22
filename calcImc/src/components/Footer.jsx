ef8ee328b83cbaa82db46194052a04d4fe91817c
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-22 08:32:20 -0300

    Update; Footer colors alterado

--- calcImc/src/components/Footer.jsx
+++ calcImc/src/components/Footer.jsx
@@ -4,28 +4,28 @@ import { auth } from "../firebaseConfig";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 const Footer = () => {
   return (
-    <footer className="tracking-wide bg-gradient-to-br from-gray-100 via-blue-50 to-white pt-2 pb-2 px-10">
+    <footer className="tracking-wide bg-gradient-to-br from-gray-100 via-blue-50 to-white pt-2 pb-2 px-10 border-gray-900">
       <div className="max-w-screen-xl mx-auto text-center justify-center items-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 pt-12">
           <div>
-            <Link to="/" className="text-slate-400 font-medium text-base mb-2">Início</Link>
+            <Link to="/" className="text-gray-900 font-medium text-base mb-2">Início</Link>
           </div>
 
           <div>
-            <Link to="/calculator" className="text-slate-400 font-medium text-base mb-2">Calculadora</Link>
+            <Link to="/calculator" className="text-gray-900 font-medium text-base mb-2">Calculadora</Link>
           </div>
 
           <div>
-            <Link to="/login" className="text-slate-400 font-medium text-base mb-2">Entrar</Link>
+            <Link to="/login" className="text-gray-900 font-medium text-base mb-2">Entrar</Link>
           </div>
 
           <div>
-            <Link to="/signup" className="text-slate-400 font-medium text-base mb-2">Registrar</Link>
+            <Link to="/signup" className="text-gray-900 font-medium text-base mb-2">Registrar</Link>
           </div>
         </div>
 
         <div className="border-t text-left border-gray-700 mb-4 pt-6 mt-8">
-          <p className="text-slate-400 text-sm">
+          <p className="text-gray-900 text-sm">
             © SoroFitness - Brasil. Todos os direitos reservados.
           </p>
         </div>
