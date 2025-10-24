e5ff261cd6ccaf185f2031c0803d2730e3962802
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-24 08:30:52 -0300

    Teste

--- calcImc/src/components/Home.jsx
+++ calcImc/src/components/Home.jsx
@@ -132,7 +132,7 @@ const Home = () => {
 
       <section
         id="info"
-        className="py-20 px-6 bg-gradient-to-b from-neutral-100 to-blue-100"
+        className="py-20 px-6 bg-gradient-to-b from-white via-neutral-200 to-blue-300"
       >
         <div className="max-w-6xl mx-auto">
           <div className="grid md:grid-cols-2 gap-12 items-center">
@@ -143,11 +143,11 @@ const Home = () => {
                 </h2>
               </div>
 
-              <blockquote className="border-l-4 border-pink-600 pl-6 text-xl text-slate-900 italic leading-loose">
+              <blockquote className="border-l-4 border-pink-600 pl-6 text-xl font-semibold text-slate-900 italic leading-loose">
                 O Índice de Massa Corporal é uma medida internacional usada para calcular se uma pessoa está no peso ideal. Ele é calculado dividindo o peso pela altura ao quadrado.
               </blockquote>
 
-              <blockquote className="border-l-4 border-pink-600 pl-6 text-xl text-slate-900 italic leading-loose">
+              <blockquote className="border-l-4 border-pink-600 pl-6 text-xl font-semibold text-slate-900 italic leading-loose">
                 Embora o IMC não seja uma medida perfeita, é uma ferramenta útil para avaliar se seu peso está dentro de uma faixa saudável e identificar possíveis riscos à saúde.
               </blockquote>
             </div>
