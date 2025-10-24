6e6c1c64ccddbc28e8e9ee791a6ff83495d1852e
Gustavo Silva <129549906+gustahxn@users.noreply.github.com>
2025-10-24 10:16:47 -0300

    tracking wider no h3 da tabela

--- calcImc/src/components/Home.jsx
+++ calcImc/src/components/Home.jsx
@@ -154,7 +154,7 @@ const Home = () => {
             </div>
 
             <div className="bg-white p-8 rounded-2xl shadow-xl">
-              <h3 className="text-2xl font-semibold ont-sans text-gray-900 mb-6 text-center">
+              <h3 className="text-2xl font-semibold ont-sans text-gray-900 mb-6 text-center tracking-wider">
                 Tabela de Classificação
               </h3>
               <div className="space-y-3">
